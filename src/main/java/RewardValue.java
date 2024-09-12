bf170ad13efdc108cbec3cbc66a3a20b5ce9cf7a
Aaron Beschorner <aaronbeschorner@gmail.com>
2024-09-12 14:30:55 -0400

    Added some comments to the code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,18 +3,19 @@ public class RewardValue {
 
     private double cashValue;
 
+    // Constructor for Cash Value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-
+    // Constructor for Miles Value
     public RewardValue(int milesValue) {
         this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
-
+    // Getter for Cash Value
     public double getCashValue() {
         return cashValue;
     }
-
+    // Getter for Miles Value
     public int getMilesValue() {
         return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
     }
